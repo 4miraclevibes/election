9b6e5d28097c2f846ee7e20d3d04f5ad9ca4fa3d
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-09-15 22:51:16 +0700

    c
    c

--- resources/views/layouts/landing/main.blade.php
+++ resources/views/layouts/landing/main.blade.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Quick Count</title>
-    <link rel="icon" href="{{ asset('assets/landing/images/LogoSod.png') }}" type="image/x-icon">
+    <title>Derisesni</title>
+    <link rel="icon" href="{{ asset('assets/landing/images/banner-derisesni.png') }}" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
