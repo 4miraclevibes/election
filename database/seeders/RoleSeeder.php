aaabc8939d48dc6c91a763ce5fbfeac1768335d6
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-10-15 17:40:32 +0700

    c
    c

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,16 +14,24 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('roles')->insert([
-            ['name' => 'admin'],
-            ['name' => 'user'],
-        ]);
+        // DB::table('roles')->insert([
+        //     ['name' => 'admin'],
+        //     ['name' => 'user'],
+        // ]);
+
+        // DB::table('users')->insert([
+        //     'name' => 'Admin',
+        //     'email' => 'admin@example.com',
+        //     'password' => Hash::make('password'),
+        //     'role_id' => 1,
+        // ]);
 
         DB::table('users')->insert([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
+            'name' => 'kholid hasibuan',
+            'email' => 'kholid.hasibuan35@gmail.com',
+            'password' => Hash::make('Anamkosong12'),
             'role_id' => 1,
         ]);
+
     }
 }
