8fc8229c8e345a632f8a170d7c2a75307b2cb325
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-04 01:23:19 +0700

    Update KecamatanElectionController.php

--- app/Http/Controllers/Dashboard/KecamatanElectionController.php
+++ app/Http/Controllers/Dashboard/KecamatanElectionController.php
@@ -47,7 +47,7 @@ class KecamatanElectionController extends Controller
     public function show($id)
     {
         $kecamatanElection = KecamatanElection::find($id);
-        dd($kecamatanElection->kelurahanElection->participantElections);
+        dd($kecamatanElection);
         return view('pages.dashboard.kecamatan.show', compact('kecamatanElection'));
     }
 }
