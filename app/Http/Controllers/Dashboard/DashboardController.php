fe99d438a1174d27bec551fac1dc267ac282d5aa
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-04 02:01:01 +0700

    Update DashboardController.php

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,21 +4,39 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\KecamatanElection;
-use App\Models\KelurahanElection;
-use App\Models\TpsElection;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $kecamatanData = KecamatanElection::with(['kelurahanElection' => function($query) {
-            $query->with(['tpsElection' => function($query) {
-                $query->withCount('participantElection as total_memilih');
-            }]);
-        }])
-        ->get();
-
+        if(Auth::user()->name == 'AdminTalawi'){
+            $kecamatanData = KecamatanElection::where('name', 'talawi')->with(['kelurahanElection' => function($query) {
+                $query->with(['tpsElection' => function($query) {
+                    $query->withCount('participantElection as total_memilih');
+                }]);
+            }])
+            ->get();
+        } else if(Auth::user()->name == 'adminlembahsegar'){
+            $kecamatanData = KecamatanElection::where('name', 'Lembah Segar')->with(['kelurahanElection' => function($query) {
+                $query->with(['tpsElection' => function($query) {
+                    $query->withCount('participantElection as total_memilih');
+                }]);
+            }])->get();
+        } else if(Auth::user()->name == 'adminsilungkang'){
+            $kecamatanData = KecamatanElection::where('name', 'SILUNGKANG')->with(['kelurahanElection' => function($query) {
+                $query->with(['tpsElection' => function($query) {
+                    $query->withCount('participantElection as total_memilih');
+                }]);
+            }])->get();
+        } else {
+            $kecamatanData = KecamatanElection::with(['kelurahanElection' => function($query) {
+                $query->with(['tpsElection' => function($query) {
+                    $query->withCount('participantElection as total_memilih');
+                }]);
+            }])->get();
+        }
+        
         $kecamatanData->each(function ($kecamatan) {
             $kecamatan->total_pemilih = $kecamatan->kelurahanElection->sum(function ($kelurahan) {
                 return $kelurahan->tpsElection->sum('total_invitation');
