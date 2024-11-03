a8e8317e43b5ae52c5680d1f47d73a03daa93a22
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-04 01:36:20 +0700

    Update KecamatanElection.php

--- app/Models/KecamatanElection.php
+++ app/Models/KecamatanElection.php
@@ -28,17 +28,9 @@ class KecamatanElection extends Model
 
     public function participantElections()
     {
-        return $this->hasManyThrough(
-            ParticipantElection::class,
-            KelurahanElection::class,
-            'kecamatan_election_id', // Foreign key di KelurahanElection
-            'tps_election_id',       // Foreign key di ParticipantElection
-            'id',                    // Local key di KecamatanElection
-            'id'                     // Local key di KelurahanElection
-        )->join('tps_elections', function($join) {
-            $join->on('tps_elections.id', '=', 'participant_elections.tps_election_id')
-                 ->whereColumn('tps_elections.kelurahan_election_id', '=', 'kelurahan_elections.id');
-        });
+        return $this->through('kelurahanElections')
+                    ->has('tpsElection')
+                    ->has('participantElection');
     }
 
     public function totalParticipant()
