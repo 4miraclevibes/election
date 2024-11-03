accef7fc705c138bb2b2066b7792361cd350afed
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-04 01:57:11 +0700

    c
    c

--- app/Http/Controllers/Dashboard/TpsElectionController.php
+++ app/Http/Controllers/Dashboard/TpsElectionController.php
@@ -9,23 +9,57 @@ use App\Models\TpsElectionDetail;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\Auth;
 class TpsElectionController extends Controller
 {
     public function index()
     {
         Log::info('Memulai fungsi index');
-        
-        $tpsElections = TpsElection::with(['kelurahanElection', 'user', 'tpsElectionDetails.user'])->get();
+        if(Auth::user()->name == 'AdminTalawi'){
+            $tpsElections = TpsElection::whereHas('kelurahanElection', function($query) {
+                $query->whereHas('kecamatanElection', function($query) {
+                    $query->where('name', 'talawi');
+                });
+            })->get();
+        } else if(Auth::user()->name == 'adminlembahsegar'){
+            $tpsElections = TpsElection::whereHas('kelurahanElection', function($query) {
+                $query->whereHas('kecamatanElection', function($query) {
+                    $query->where('name', 'Lembah Segar');
+                });
+            })->get();
+        } else if(Auth::user()->name == 'adminsilungkang'){
+            $tpsElections = TpsElection::whereHas('kelurahanElection', function($query) {
+                $query->whereHas('kecamatanElection', function($query) {
+                    $query->where('name', 'SILUNGKANG');
+                });
+            })->get();
+        } else {
+            $tpsElections = TpsElection::all();
+        }
         Log::info('TpsElections diambil: ' . $tpsElections->count());
         
-        $kelurahanElections = KelurahanElection::with([
-            'user',
-            'kecamatanElection',
-            'tpsElection',
-            'participantElections',
-            'kelurahanDetails'
-        ])->get();
+        if(Auth::user()->name == 'AdminTalawi'){
+            $kelurahanElections = KelurahanElection::whereHas('kecamatanElection', function($query) {
+                $query->where('name', 'talawi');
+            })->get();
+        } else if(Auth::user()->name == 'adminlembahsegar'){
+            $kelurahanElections = KelurahanElection::whereHas('kecamatanElection', function($query) {
+                $query->where('name', 'Lembah Segar');
+            })->get();
+        } else if(Auth::user()->name == 'adminsilungkang'){
+            $kelurahanElections = KelurahanElection::whereHas('kecamatanElection', function($query) {
+                $query->where('name', 'SILUNGKANG');
+            })->get();
+        } else {
+            $kelurahanElections = KelurahanElection::with([
+                'user',
+                'kecamatanElection',
+                'tpsElection',
+                'participantElections',
+                'kelurahanDetails'
+            ])->get();
+        }
+
         Log::info('KelurahanElections diambil: ' . $kelurahanElections->count());
         
         $users = User::where('role_id', 2)
